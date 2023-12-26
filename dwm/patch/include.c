d5ff86751460220ab79189cb3666b59ee220cde5
DannyHyde <danny@dannyhyde.co.uk>
2023-12-26 04:03:47 +0000

    Iitial commit after building from flexipatch

---
+++ dwm/patch/include.c
@@ -0,0 +1,25 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_alpha.c"
+#include "bar_dwmblocks.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_statuscmd.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "attachx.c"
+#include "autostart.c"
+#include "pertag.c"
+#include "stacker.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
